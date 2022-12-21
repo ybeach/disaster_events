f058c748b1f9f4f580f591f12355a0a3467082bf
ybeach <taizo.yamada@gmail.com>
2022-12-21 22:13:28 +0900

    mod nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
             
         },
         pageTransition: { name: 'page', mode: 'out-in' },
-        baseURL: "/collection/digitalgallery/disaster_events/",
+        baseURL: "/collection/degitalgallary/disaster_events/",
     },
     /*
         router: {

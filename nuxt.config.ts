da98535a2946dea5545c0a0dd1f7c15dc41cedce
ybeach <taizo.yamada@gmail.com>
2023-02-02 22:20:33 +0900

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 export default defineNuxtConfig({
     srcDir: 'src/',
+    link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
     css: ['@/assets/css/reset.css', '@/assets/css/style.css'],
     app: {
         head: {
